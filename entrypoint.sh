70130ae17ca4a95095ce31b1a7feba39eb167a54
sist <15859432+stiebels@users.noreply.github.com>
2023-07-26 23:54:09 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,11 @@ export INPUT_PY2PUML_VERSION=${4}
 if [ -z "$INPUT_PY2PUML_VERSION" ]; then
 	pip install py2puml
 else
-	pip install py2puml==${INPUT_PY2PUML_VERSION}
+	pip install py2puml=="${INPUT_PY2PUML_VERSION}"
 fi
 
-py2puml ${INPUT_PATH} ${INPUT_MODULE} > ${INPUT_PUML_OUTPUT_PATH}
+py2puml "${INPUT_PATH}" "${INPUT_MODULE}" > "${INPUT_PUML_OUTPUT_PATH}"
+echo $(ls)
 
 
 if [ -z "$INPUT_VERSION" ]; then
@@ -20,4 +21,4 @@ else
 	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.${INPUT_PUML_VERSION}.jar/download"
 fi
 
-java -jar /tmp/plantuml.jar "-tpng "${INPUT_PUML_OUTPUT_PATH}
+java -jar /tmp/plantuml.jar "-tpng ""${INPUT_PUML_OUTPUT_PATH}"
