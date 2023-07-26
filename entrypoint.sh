403cf8dfec3f6a7f19c10577b88e6dfeceadc062
sist <15859432+stiebels@users.noreply.github.com>
2023-07-27 01:04:27 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/sh -l
 
-INPUT_PUML_OUTPUT_PATH="${1}"
+INPUT_OUTPUT_DIR="${1}"
 INPUT_PATH="${2}"
 INPUT_MODULE="${3}"
 INPUT_PY2PUML_VERSION="${4}"
 
 
-if [ -z "$INPUT_PY2PUML_VERSION" ]; then
+if [ "$INPUT_PY2PUML_VERSION" = "latest" ]; then
 	pip install py2puml
 else
 	pip install py2puml=="${INPUT_PY2PUML_VERSION}"
 fi
 
-py2puml "${INPUT_PATH}" "${INPUT_MODULE}" > "${INPUT_PUML_OUTPUT_PATH}"
+mkdir -p "${INPUT_OUTPUT_DIR}"
+py2puml "${INPUT_PATH}" "${INPUT_MODULE}" > "${INPUT_OUTPUT_DIR}"/"${INPUT_MODULE}".puml
 
 if [ -z "$INPUT_VERSION" ]; then
 	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"
@@ -20,4 +21,4 @@ else
 	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.${INPUT_PUML_VERSION}.jar/download"
 fi
 
-java -jar /tmp/plantuml.jar "${INPUT_PUML_OUTPUT_PATH}"
+java -jar /tmp/plantuml.jar "${INPUT_OUTPUT_DIR}"/"${INPUT_MODULE}".puml -o ../"${INPUT_OUTPUT_DIR}"
