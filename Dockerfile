1914c7e8cb776685796b1849aff8a33e22322ce5
sist <15859432+stiebels@users.noreply.github.com>
2023-07-26 23:48:46 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM alpine:3.11
 
-RUN apk add --no-cache openjdk11-jre graphviz ttf-droid ttf-droid-nonlatin
-
+RUN apk add --no-cache openjdk11-jre graphviz ttf-droid ttf-droid-nonlatin python3
 COPY entrypoint.sh /entrypoint.sh
-
 ENTRYPOINT ["/entrypoint.sh"]
-
 CMD [ "-h" ]
+
+# Install python/pip
+ENV PYTHONUNBUFFERED=1
+RUN ln -sf python3 /usr/bin/python
+RUN python3 -m ensurepip
+RUN pip3 install --no-cache --upgrade pip
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/sh -l
 
+export INPUT_PUML_OUTPUT_PATH=${1}
+export INPUT_PATH=${2}
+export INPUT_MODULE=${3}
+export INPUT_PY2PUML_VERSION=${4}
+
+if [ -z "$INPUT_PY2PUML_VERSION" ]; then
+	pip install py2puml
+else
+	pip install py2puml==${INPUT_PY2PUML_VERSION}
+fi
+
+py2puml ${INPUT_PATH} ${INPUT_MODULE} > ${INPUT_PUML_OUTPUT_PATH}
+
+
 if [ -z "$INPUT_VERSION" ]; then
 	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"
 else
-	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.${INPUT_VERSION}.jar/download"
+	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.${INPUT_PUML_VERSION}.jar/download"
 fi
 
-java -jar /tmp/plantuml.jar "$@"
+java -jar /tmp/plantuml.jar "-tpng "${INPUT_PUML_OUTPUT_PATH}
