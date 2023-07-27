5e8d5ee56d339797e4d9b5ce9424f359a433fab6
sist <15859432+stiebels@users.noreply.github.com>
2023-07-27 17:55:21 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
 
-INPUT_OUTPUT_DIR="${1}"
-INPUT_PATH="${2}"
-INPUT_MODULE="${3}"
-INPUT_PY2PUML_VERSION="${4}"
+INPUT_PUML_VERSION="${1}"
+INPUT_PY2PUML_VERSION="${2}"
+INPUT_PATH="${3}"
+INPUT_MODULE="${4}"
+INPUT_OUTPUT_DIR="${5}"
 
 
 if [ "$INPUT_PY2PUML_VERSION" = "latest" ]; then
@@ -15,7 +16,8 @@ fi
 mkdir -p "${INPUT_OUTPUT_DIR}"
 py2puml "${INPUT_PATH}" "${INPUT_MODULE}" > "${INPUT_OUTPUT_DIR}"/"${INPUT_MODULE}".puml
 
-if [ -z "$INPUT_VERSION" ]; then
+
+if [ "$INPUT_PUML_VERSION" = "latest" ]; then
 	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.jar/download"
 else
 	wget -O /tmp/plantuml.jar "http://sourceforge.net/projects/plantuml/files/plantuml.${INPUT_PUML_VERSION}.jar/download"
